b9e5a66d66b629b1720997c40de3dcced5375d6a
aarkasko <aaro.koinsaari@proton.me>
2023-11-03 13:36:16 +0200

    Add update method test

--- MovieDatabase/src/test/java/com/moviedb/dao/MovieDaoFilledDBTest.java
+++ MovieDatabase/src/test/java/com/moviedb/dao/MovieDaoFilledDBTest.java
@@ -47,7 +47,7 @@ public class MovieDaoFilledDBTest extends FilledDBSetup {
         int movieId = 2;  // The Wolf of Wall Street
         Movie retrievedMovie = dao.read(movieId);
 
-        assertNotNull(retrievedMovie);
+        assertNotNull(retrievedMovie); // Confirm that the movie has been added to the database
         assertEquals(movieId, retrievedMovie.getId());
         assertEquals("The Wolf of Wall Street", retrievedMovie.getTitle());
         assertEquals(2013, retrievedMovie.getReleaseYear());
@@ -58,5 +58,36 @@ public class MovieDaoFilledDBTest extends FilledDBSetup {
         List<Integer> expectedGenreIds = Arrays.asList(3, 5); // Comedy, Drama
         assertEquals(expectedActorIds, retrievedMovie.getActorIds());
         assertEquals(expectedGenreIds, retrievedMovie.getGenreIds());
+    }
+
+
+    @Test
+    void update() {
+        // Fetch the original movie
+        MovieDao dao = new MovieDao(connection);
+        Movie originalMovie = dao.read(3);  // Django Unchained
+
+        int originalMovieId = originalMovie.getId();
+
+        // Define new attributes for the movie
+        String newTitle = "Django The Movie";
+        int newReleaseYear = 2023;
+        String newDirector = "Aki Kaurismäki";
+        List<Integer> newActorIds = Arrays.asList(1, 5); // Robert De Niro, Margot Robbie
+        List<Integer> newGenreIds = Arrays.asList(6, 7); // Fantasy, Historical
+
+        // Updated movie object with new attributes
+        Movie updatedMovie = new Movie(newTitle, newReleaseYear, newDirector, newActorIds, newGenreIds);
+        updatedMovie.setId(originalMovieId); // Link updated object to existing record
+
+        assertTrue(dao.update(updatedMovie)); // Update the movie to database and confirm it
+
+        // Check that the update was correctly implemented
+        Movie fetchedUpdatedMovie = dao.read(originalMovieId);
+        assertEquals(newTitle, fetchedUpdatedMovie.getTitle());
+        assertEquals(newReleaseYear, fetchedUpdatedMovie.getReleaseYear());
+        assertEquals(newDirector, fetchedUpdatedMovie.getDirector());
+        assertEquals(newActorIds, fetchedUpdatedMovie.getActorIds());
+        assertEquals(newGenreIds, fetchedUpdatedMovie.getGenreIds());
     }
 }
