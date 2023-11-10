364da4783dab1a0a72abc282c1f86bfa3de5ca72
Aaro Koinsaari <aaro.koinsaari@proton.me>
2023-11-10 18:59:44 +0200

    Refactor tests for empty database
    Modify tests to focus on behavior with empty database and therefore for non-existent data.

--- MovieDatabase/src/test/java/com/moviedb/dao/MovieDaoEmptyDBTest.java
+++ MovieDatabase/src/test/java/com/moviedb/dao/MovieDaoEmptyDBTest.java
@@ -13,29 +13,31 @@ import com.moviedb.models.Movie;
 
 /**
  * This class contains unit tests for the MovieDao class using an empty database setup.
+ * Only genres are existing in the empty database since they are static.
  * Each test method is designed to test a single functionality of the MovieDao class.
  */
 class MovieDaoEmptyDBTest extends EmptyDBSetup {
+
+    private MovieDao dao;
 
     /**
      * Additional setup for the empty database for each test.
      */
     @BeforeEach
     public void setUp() {
+        dao = new MovieDao(connection);
         addActorsToDB(10);  // Add 10 test actors to database
     }
 
 
     @Test
-    void create() {
+    void createTest() {
         // Create new test movie
         String title = "Test Movie";
         int releaseYear = 2023;
         String director = "Test Director";
-        List<Integer> actorIds = new ArrayList<>();
-        List<Integer> genreIds = new ArrayList<>();
-        actorIds.addAll(Arrays.asList(1, 2, 3, 4, 5));
-        genreIds.addAll(Arrays.asList(1, 2, 3, 4));
+        List<Integer> actorIds = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        List<Integer> genreIds = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
         Movie testMovie = new Movie(title, releaseYear, director, actorIds, genreIds);
 
         MovieDao dao = new MovieDao(connection);
@@ -55,95 +57,38 @@ class MovieDaoEmptyDBTest extends EmptyDBSetup {
 
 
     @Test
-    void read() {
-        // Create new test movie
-        String title = "Test Movie";
-        int releaseYear = 2023;
-        String director = "Test Director";
-        List<Integer> actorIds = new ArrayList<>();
-        List<Integer> genreIds = new ArrayList<>();
-        actorIds.addAll(Arrays.asList(1, 2, 3));
-        genreIds.addAll(Arrays.asList(1, 2));
-        Movie testMovie = new Movie(title, releaseYear, director, actorIds, genreIds);
-
-        MovieDao dao = new MovieDao(connection);
-        int generatedId = dao.create(testMovie);
-
-        // Read the movie from the database
-        Movie fetchedMovie = dao.read(generatedId);
-
-        // Confirm that the fetched movie information is correct
-        assertEquals(title, fetchedMovie.getTitle());
-        assertEquals(releaseYear, fetchedMovie.getReleaseYear());
-        assertEquals(director, fetchedMovie.getDirector());
-        assertEquals(actorIds, fetchedMovie.getActorIds());
-        assertEquals(genreIds, fetchedMovie.getGenreIds());
-        assertEquals(generatedId, fetchedMovie.getId());
+    void readTest() {
+        int nonExistentMovieId = 99;
+        Movie fetchedMovie = dao.read(nonExistentMovieId);
+        assertNull(fetchedMovie, "Should return null for trying to read a non-existent movie");
     }
 
 
     @Test
-    void update() {
-        // Create new test movie
-        String originalTitle = "Original Movie";
-        int originalReleaseYear = 2020;
-        String originalDirector = "Original Director";
-        List<Integer> originalActorIds = Arrays.asList(1, 2, 3);
-        List<Integer> originalGenreIds = Arrays.asList(1, 2);
-        Movie originalMovie = new Movie(originalTitle, originalReleaseYear, originalDirector, originalActorIds, originalGenreIds);
+    void updateTest() {
+        int nonExistentMovieId = 99;
 
-        MovieDao dao = new MovieDao(connection);
-        int movieId = dao.create(originalMovie);
-
-        // Update the test movie information
+        // Create non-existent movie
         String updatedTitle = "Updated Movie";
         int updatedReleaseYear = 2023;
         String updatedDirector = "Updated Director";
         List<Integer> updatedActorIds = Arrays.asList(3, 4, 5);
         List<Integer> updatedGenreIds = Arrays.asList(2, 3);
         Movie updatedMovie = new Movie(updatedTitle, updatedReleaseYear, updatedDirector, updatedActorIds, updatedGenreIds);
-        updatedMovie.setId(movieId);
+        updatedMovie.setId(nonExistentMovieId);
 
+        // Try update and assert that the update fails
         boolean updateResult = dao.update(updatedMovie);
-        assertTrue(updateResult);
-
-        // Read the updated movie from the database
-        Movie fetchedUpdatedMovie = dao.read(movieId);
-
-        // Confirm that the fetched movie information has been updated
-        assertEquals(updatedTitle, fetchedUpdatedMovie.getTitle());
-        assertEquals(updatedReleaseYear, fetchedUpdatedMovie.getReleaseYear());
-        assertEquals(updatedDirector, fetchedUpdatedMovie.getDirector());
-        assertEquals(updatedActorIds, fetchedUpdatedMovie.getActorIds());
-        assertEquals(updatedGenreIds, fetchedUpdatedMovie.getGenreIds());
+        assertFalse(updateResult, "Should return false for trying to update a non-existent movie");
     }
 
 
     @Test
-    void delete() {
-        // Create new movie to the database
-        String title = "Test Movie";
-        int releaseYear = 2023;
-        String director = "Test Director";
-        List<Integer> actorIds = new ArrayList<>();
-        List<Integer> genreIds = new ArrayList<>();
-        actorIds.addAll(Arrays.asList(1, 2, 3));
-        genreIds.addAll(Arrays.asList(1, 2));
-        Movie testMovie = new Movie(title, releaseYear, director, actorIds, genreIds);
+    void deleteTest() {
+        int nonExistentMovieId = 99;
 
-        MovieDao dao = new MovieDao(connection);
-        int generatedId = dao.create(testMovie);
-
-        // Confirm that the movie is in the database
-        Movie fetchedMovie = dao.read(generatedId);
-        assertNotNull(fetchedMovie);
-
-        // Try to delete the movie
-        boolean isDeleted = dao.delete(generatedId);
-        assertTrue(isDeleted);
-
-        // Confirm that the deleted movie is no longer in the database
-        Movie deletedMovie = dao.read(generatedId);
-        assertNull(deletedMovie);
+        // Try deleting non-existent movie and assert that the deletion fails
+        boolean isDeleted = dao.delete(nonExistentMovieId);
+        assertFalse(isDeleted, "Should return false for trying to delete a non-existent movie");
     }
 }
