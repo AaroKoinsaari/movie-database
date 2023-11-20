152d853d4f902d1736793ba341170d87fd6fdd5b
Aaro Koinsaari <aaro.koinsaari@proton.me>
2023-11-20 18:32:59 +0200

    Refactor movie update with link management
    - Optimized link update logic by replacing the deletion and re-addition of all actor and genre links in the update method with more efficient logic. Now, the method only updates the links that have actually changed, which reduces unnecessary database operations.
    
    - Introduced updateMovieLinks method to handle the addition and removal of movie-related links (actors and genres). This method efficiently adds new links and removes outdated ones based on the updated movie information. By splitting the link management into separate methods, the code's readability and maintainability are also improved.
    
    - Implemented transactional integrity, which ensures that all database operations within the update method are part of a single transaction. This ensures that all updates are either fully completed or fully rolled back, which maintains the integrity of the database state.
    
    - Eliminated repetitive code patterns, especially in the handling of genre and actor links, leading to a more concise and efficient method.

--- MovieDatabase/src/main/java/com/moviedb/dao/MovieDao.java
+++ MovieDatabase/src/main/java/com/moviedb/dao/MovieDao.java
@@ -1,7 +1,9 @@
 package com.moviedb.dao;
 
-import java.util.Arrays;
+import java.util.Set;
 import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.ArrayList;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -204,64 +206,156 @@ public class MovieDao {
      * @param updatedMovie The movie object containing updated details with the correct ID of the movie to be updated.
      * @return boolean true if the update was successful, otherwise false.
      * @throws SQLException If there's an error during the database operation.
-     *
-     * TODO:
-     * - Modify deleting and inserting into their own methods
-     * - Improve efficiency by checking first if dependencies need updating at all
      */
-    public boolean update(Movie updatedMovie) {
+    public boolean update(Movie updatedMovie) throws SQLException {
         boolean updateSuccessful = false;
-
-        // Define all the SQL queries
-        String sqlUpdateMovie = "UPDATE movies SET title = ?, release_year = ?, director = ? WHERE id = ?";
-        String sqlDeleteGenre = "DELETE FROM movie_genres WHERE movie_id = ?";
-        String sqlInsertGenre = "INSERT INTO movie_genres(movie_id, genre_id) VALUES(?, ?)";
-        String sqlDeleteActor = "DELETE FROM movie_actors WHERE movie_id = ?";
-        String sqlInsertActor = "INSERT INTO movie_actors(movie_id, actor_id) VALUES(?, ?)";
+        Movie existingMovie = read(updatedMovie.getId());
 
         try {
-            // Update the main movie details
-            try (PreparedStatement pstmt = connection.prepareStatement(sqlUpdateMovie)) {
-                pstmt.setString(1, updatedMovie.getTitle());
-                pstmt.setInt(2, updatedMovie.getReleaseYear());
-                pstmt.setString(3, updatedMovie.getDirector());
-                pstmt.setInt(4, updatedMovie.getId());
-                pstmt.executeUpdate();
+            connection.setAutoCommit(false);
+
+            // Update movie details if they are updated
+            if (!existingMovie.getTitle().equals(updatedMovie.getTitle()) ||
+                    existingMovie.getReleaseYear() != updatedMovie.getReleaseYear() ||
+                    !existingMovie.getDirector().equals(updatedMovie.getDirector())) {
+                updateMovieMainDetails(updatedMovie);
             }
 
-            // Delete old genre dependencies and add new ones
-            try (PreparedStatement pstmtDelete = connection.prepareStatement(sqlDeleteGenre)) {
-                pstmtDelete.setInt(1, updatedMovie.getId());
-                pstmtDelete.executeUpdate();
+            // Update the actor links if they have changed
+            if (!existingMovie.getActorIds().equals(updatedMovie.getActorIds())) {
+                updateMovieLinks(updatedMovie.getId(), new HashSet<>(updatedMovie.getActorIds()), "movie_actors", "actor_id");
             }
 
-            try (PreparedStatement pstmtInsert = connection.prepareStatement(sqlInsertGenre)) {
-                for (Integer genreId : updatedMovie.getGenreIds()) {
-                    pstmtInsert.setInt(1, updatedMovie.getId());
-                    pstmtInsert.setInt(2, genreId);
-                    pstmtInsert.executeUpdate();
-                }
+            // Update the genre links if they have changed
+            if (!existingMovie.getGenreIds().equals(updatedMovie.getGenreIds())) {
+                updateMovieLinks(updatedMovie.getId(), new HashSet<>(updatedMovie.getGenreIds()), "movie_genres", "genre_id");
             }
 
-            // Delete old actor dependencies and add new ones
-            try (PreparedStatement pstmtDelete = connection.prepareStatement(sqlDeleteActor)) {
-                pstmtDelete.setInt(1, updatedMovie.getId());
-                pstmtDelete.executeUpdate();
-            }
-
-            try (PreparedStatement pstmtInsert = connection.prepareStatement(sqlInsertActor)) {
-                for (Integer actorId : updatedMovie.getActorIds()) {
-                    pstmtInsert.setInt(1, updatedMovie.getId());
-                    pstmtInsert.setInt(2, actorId);
-                    pstmtInsert.executeUpdate();
-                }
-            }
-
-            updateSuccessful = true;  // No exceptions, update was succesful
+            connection.commit();
+            updateSuccessful = true;
         } catch (SQLException e) {
-            e.printStackTrace();
+            connection.rollback();  // Undo changes if there's an error
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("Error Code: " + e.getErrorCode());
+            System.out.println("Message: " + e.getMessage());
+        } finally {
+            connection.setAutoCommit(true);
         }
+
         return updateSuccessful;
+    }
+
+
+    /**
+     * Updates the main details of a specified movie in the database (title, release year and director).
+     *
+     * @param updatedMovie The Movie object containing the new details.
+     * @throws SQLException If there's an error during the database operation.
+     */
+    private void updateMovieMainDetails(Movie updatedMovie) throws SQLException {
+        String sql = "UPDATE movies SET title = ?, release_year = ?, director = ? WHERE id = ?";
+
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, updatedMovie.getTitle());
+            pstmt.setInt(2, updatedMovie.getReleaseYear());
+            pstmt.setString(3, updatedMovie.getDirector());
+            pstmt.executeUpdate();
+        }
+    }
+
+
+    /**
+     * Updates the link associations for a movie in the database by adding new links
+     * and removing outdated ones for a specific Movie.
+     *
+     * @param movieId The ID of the movie for which links are being updated.
+     * @param updatedIds A set of new IDs (either actor or genre IDs) to be associated with the movie.
+     * @param table The name of the table where the links are stored (either 'movie_actors' or 'movie_genres').
+     * @param idColumn The name of the column in the join table that stores the IDs (either 'actor_id' or 'genre_id').
+     * @throws SQLException If there's an error during the database operation.
+     */
+    private void updateMovieLinks(int movieId, Set<Integer> updatedIds, String table, String idColumn) throws SQLException {
+        Set<Integer> currentIds = getCurrentIds(movieId, table, idColumn);
+
+        // Delete links that are not on the updated list
+        for (Integer id : currentIds) {
+            if (!updatedIds.contains(id)) {
+                removeLinkFromMovie(movieId, id, table, idColumn);
+            }
+        }
+
+        // Add new links
+        for (Integer id : updatedIds) {
+            if (!currentIds.contains(id)) {
+                addLinkToMovie(movieId, id, table, idColumn);
+            }
+        }
+    }
+
+
+    /**
+     * Retrieves a set of current IDs linked with a specific movie from a join table.
+     *
+     * @param movieId The ID of the movie for which linked IDs are being fetched.
+     * @param table The name of the join table from which to fetch the links (either 'movie_actors' or 'movie_genres').
+     * @param idColumn The name of the column in the join table that stores the IDs ('actor_id' or 'genre_id').
+     * @return A set of integer IDs currently linked with the movie in the specified join table.
+     * @throws SQLException If there's an error during the database operation.
+     */
+    private Set<Integer> getCurrentIds(int movieId, String table, String idColumn) throws SQLException {
+        Set<Integer> ids = new HashSet<>();
+
+        String sql = "SELECT " + idColumn + " FROM " + table + " WHERE movie_id = ?";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, movieId);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    ids.add(rs.getInt(idColumn));
+                }
+            }
+        }
+        return ids;
+    }
+
+
+    /**
+     * Removes a specific link between a movie and an actor or genre in the database.
+     *
+     * @param movieId The ID of the movie from which the link is being removed.
+     * @param id The ID of the linked entity (actor ID or genre ID) to be removed.
+     * @param table The name of the join table (either 'movie_actors' or 'movie_genres').
+     * @param idColumn The name of the column in the join table that stores the actor's
+     *                 or genre's IDs (either 'actor_id' or 'genre_id').
+     * @throws SQLException If there's an error during the database operation.
+     */
+    private void removeLinkFromMovie(int movieId, int id, String table, String idColumn) throws SQLException {
+        String sql = "DELETE FROM " + table + " WHERE movie_id = ? AND " + idColumn + " = ?";
+
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, movieId);
+            pstmt.setInt(2, id);
+            pstmt.executeUpdate();
+        }
+    }
+
+
+    /**
+     * Adds a new link between a movie and an actor or genre in the database.
+     *
+     * @param movieId The ID of the movie to which the link is being added.
+     * @param id The ID of the associated entity (actor ID or genre ID) to be added.
+     * @param table The name of the join table ('movie_actors' or 'movie_genres').
+     * @param idColumn The name of the column in the join table that stores the actor's
+     *                 or genre's IDs ('actor_id' or 'genre_id').
+     * @throws SQLException If there's an error during the database operation.
+     */
+    private void addLinkToMovie(int movieId, int id, String table, String idColumn) throws SQLException {
+        String sql = "INSERT INTO " + table + " (movie_id, " + idColumn + ") VALUES (?, ?)";
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, movieId);
+            pstmt.setInt(2, id);
+            pstmt.executeUpdate();
+        }
     }
 
 
