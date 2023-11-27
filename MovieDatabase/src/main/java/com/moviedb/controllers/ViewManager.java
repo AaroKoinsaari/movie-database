d3a9ba4f53d8dcdbecb49bef69ddac7e5528b5ee
Aaro Koinsaari <aaro.koinsaari@proton.me>
2023-11-27 21:36:06 +0200

    Enhanced dialog handling and added documentation

--- MovieDatabase/src/main/java/com/moviedb/controllers/ViewManager.java
+++ MovieDatabase/src/main/java/com/moviedb/controllers/ViewManager.java
@@ -12,10 +12,15 @@ import java.sql.Connection;
 
 public class ViewManager {
 
-    Connection connection;
-    Movie currentMovie;
 
-    public static void openActorDialog(Movie currentMovie, Connection connection, Stage ownerStage) throws IOException {
+    /**
+     * Opens the 'Add Actor' dialog.
+     * Loads the ActorDialogView FXML, sets up the controller, and displays the dialog in a modal window.
+     * Passes the current database connection and selected movie to the dialog controller.
+     * After the dialog is closed, updates the movie details with any changes made.
+     */
+    public static void openActorDialog(Movie currentMovie, Connection connection, Stage ownerStage)
+        throws IOException {
         // Load the FXML file
         FXMLLoader loader = new FXMLLoader(ViewManager.class.getResource("/views/ActorDialogView.fxml"));
         Parent root = loader.load();
@@ -34,5 +39,38 @@ public class ViewManager {
         dialogStage.initModality(Modality.APPLICATION_MODAL);
         dialogStage.initOwner(ownerStage);
         dialogStage.showAndWait();  // Wait until the user closes the window
+    }
+
+
+    /**
+     * Opens the 'Add Genre' dialog.
+     * Loads the GenreDialogView FXML, sets up the controller, and displays the dialog in a modal window.
+     * Provides the current movie and database connection to the dialog controller.
+     */
+    public static void openGenreDialog(Movie currentMovie, Connection connection, Stage ownerStage)
+        throws IOException {
+        // Load the FXML file
+        FXMLLoader loader = new FXMLLoader(ViewManager.class.getResource("/views/GenreDialogView.fxml"));
+        Parent root = loader.load();
+
+        GenreDialogViewController controller = loader.getController();
+        controller.setCurrentMovie(currentMovie);
+        controller.setConnection(connection);  // Pass the current connection
+
+        // Create new scene and stage
+        Scene scene = new Scene(root);
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle("Choose the Genres");
+        dialogStage.setScene(scene);
+
+        // Set the stage as modal
+        dialogStage.initModality(Modality.APPLICATION_MODAL);
+        dialogStage.initOwner(ownerStage);
+        dialogStage.showAndWait();  // Wait until the user closes the window
+    }
+
+
+    public static void openMovieDialog(Movie currentMovie, Connection connection, Stage ownerStage) {
+        
     }
 }
